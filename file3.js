9f1720870f0594d987ed84644e8929bb7a98b504
Sabidharshini125 <115529650+Sabidharshini125@users.noreply.github.com>
2024-02-29 18:16:24 +0530

    Update file3.js

--- file3.js
+++ file3.js
@@ -2,7 +2,7 @@ function Solve(val) {
     var v = document.getElementById('res');
     v.value += val;
  }
- function Result() {
+function Result() {
     var num1 = document.getElementById('res').value;
     var num2 = eval(num1);
     document.getElementById('res').value = num2;
